d574f1869b1fb21f7136989bdae5cdd3d4576cce
nemsi85 <valentina.carrirolo@gmail.com>
2019-03-31 01:02:05 +0100

    Experiments 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,20 @@ package com.github.jacklt.gae.ktor.tg
 
 fun main() = startApp()
 
+var old: Int? = null
+
 fun myApp(input: String): String {
-    return "Ciao $input"
+    val addendo1: Int? = input.toIntOrNull()
+    if (addendo1 != null) {
+        if (old != null) {
+            val risultato: Int = old!! + addendo1
+          old = null
+            return "Il tuo risultato è $risultato"
+        } else {
+            old = addendo1
+            return "Inserisci il secondo numero"
+        }
+    } else {
+        return "Il tuo input non è valido"
+    }
 }
